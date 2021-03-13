4e1c6d79cfc2ac05bc1e1c2def79c38a9ca565f8
Fenix1635 <lucayuki1635@gmail.com>
2021-03-13 15:53:37 -0300

    master

--- app.py
+++ app.py
@@ -9,23 +9,23 @@ app = Flask(__name__)
 cors = CORS(app, resource={r"/*":{"origins": "*"}})
 
  
-url = 'https://www.coupang.com/vp/products/4348473678/vendor-items/72771319696/shipping-consolidation-widget?vendorId=A00157129&shippingPlaceId=657033'
+# url = 'https://www.coupang.com/vp/products/4348473678/vendor-items/72771319696/shipping-consolidation-widget?vendorId=A00157129&shippingPlaceId=657033'
 
 
-headers = {
-                'authority': 'www.coupang.com',
-                "pragma": "no-cache",
-                'cache-control': 'no-cache',
-                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36',
-                'accept': '*/*',
-                'sec-fetch-dest':'empty',
-                'sec-fetch-mode':'cors',
-                'sec-fetch-site':'same-origin',
-                'referer': 'https://www.coupang.com//vp/products/4348473678?vendorItemId=72771319696&sourceType=SDP_SC_RECOMMENDATION'
-        }
+# headers = {
+#                 'authority': 'www.coupang.com',
+#                 "pragma": "no-cache",
+#                 'cache-control': 'no-cache',
+#                 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36',
+#                 'accept': '*/*',
+#                 'sec-fetch-dest':'empty',
+#                 'sec-fetch-mode':'cors',
+#                 'sec-fetch-site':'same-origin',
+#                 'referer': 'https://www.coupang.com//vp/products/4348473678?vendorItemId=72771319696&sourceType=SDP_SC_RECOMMENDATION'
+#         }
 
-response = requests.get(url, headers=headers)
-response_formatted2 = json.loads(response.content.decode('utf-8-sig').encode('utf-8'))
+# response = requests.get(url, headers=headers)
+# response_formatted2 = json.loads(response.content.decode('utf-8-sig').encode('utf-8'))
 
 
 
@@ -36,7 +36,7 @@ def index():
 
 @app.route('/api', methods=['GET'])
 def deploy():
-    return response_formatted2
+    return "<h1>Olá2<h1>"
 
 def main():
     port = int(os.environ.get("PORT", 5000))
