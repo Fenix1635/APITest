1bbcdaea4b4fb533d014fdf81b867d1164806540
Fenix1635 <lucayuki1635@gmail.com>
2021-03-13 17:43:15 -0300

    master

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
-from flask import Flask
-from flask_cors import CORS
 import requests
 import json
 import os
 from selenium import webdriver
 
-app = Flask(__name__)
-
-cors = CORS(app, resource={r"/*":{"origins": "*"}})
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
@@ -16,21 +11,5 @@ chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 driver.get('https://www.catch.com.au/product/asus-rog-strix-geforce-rtx-3090-24gb-oc-edition-graphics-card-7126834/?sid=rtx%203090&sp=1&st=32&srtrev=sj-x57l6dk9opvxnkkz8gmflc')
-test = driver.page_source
+print(driver.page_source)
 
-@app.route('/', methods=['GET'])
-
-def index():
-    return "<h1>Olá<h1>"
-
-@app.route('/api', methods=['GET'])
-def deploy():
-    return test
-
-def main():
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
-
-if __name__ == "__main__":
-    print('ONLINE')
-    main()
